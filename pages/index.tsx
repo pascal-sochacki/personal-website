d272124e431e57c153ae66197d31c471d747de0e
Pascal Sochacki <pascal@sochacki.dev>
2022-06-20 22:54:52 +0200

    add me.png

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Link from "next/link";
+import Image from "next/image";
+import me from "../public/images/me.png"
 
 const Home: NextPage = () => {
     return (
@@ -12,7 +13,8 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
-            <div className={"w-full p-5 bg-background_dark flex justify-between items-center px-36 xl:flex-row flex-col"}>
+            <div
+                className={"w-full p-5 bg-background_dark flex justify-between items-center px-36 xl:flex-row flex-col"}>
                 <Link href={"/"}>
                     <a className={"text-2xl font-azeret"}>sochacki.dev</a>
                 </Link>
@@ -31,23 +33,31 @@ const Home: NextPage = () => {
 
 
             <main className={"px-36 pt-56"}>
-                <div className={"flex gap-4"}>
-                    <Link href={"https://github.com/pascal-sochacki"}>
-                        <a className={"border-2 rounded-full px-1 border-y-d"}>Github</a>
-                    </Link>
-                    <Link href={"https://www.linkedin.com/in/paso/"}>
-                        <a className={"border-2 rounded-full px-1 border-y-d"}>LinkedIn</a>
-                    </Link>
-                    <Link href={"https://twitter.com/PascalSochacki"}>
-                        <a className={"border-2 rounded-full px-1 border-y-d"}>Twitter</a>
-                    </Link>
+                <div className={"flex items-center gap-5"}>
+                    <div>
+                    <div className={"flex gap-4"}>
+                        <Link href={"https://github.com/pascal-sochacki"}>
+                            <a className={"border-2 rounded-full px-1 border-y-d"}>Github</a>
+                        </Link>
+                        <Link href={"https://www.linkedin.com/in/paso/"}>
+                            <a className={"border-2 rounded-full px-1 border-y-d"}>LinkedIn</a>
+                        </Link>
+                        <Link href={"https://twitter.com/PascalSochacki"}>
+                            <a className={"border-2 rounded-full px-1 border-y-d"}>Twitter</a>
+                        </Link>
+                    </div>
+                    <h1 className={"text-4xl font-bold"}>
+                        I am Pascal Sochacki
+                    </h1>
+                    <p>
+                        I'm Pascal, IT Consultant focus on Cloud and DevOps.
+                    </p>
+                    </div>
+                    <div className={ "w-40 rounded-full overflow-hidden border-2 flex bg-d"}>
+                        <Image src={me} />
+                    </div>
+
                 </div>
-                <h1 className={"text-4xl font-bold"}>
-                    I am Pascal Sochacki
-                </h1>
-                <p>
-                    I'm Pascal, IT Consultant focus on Cloud and DevOps.
-                </p>
 
             </main>
         </>
